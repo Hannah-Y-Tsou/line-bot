f88623f9decc37f5228dc91cc4d2c0a6113e3cc9
鄒伃軒 <nancyzou82@gmail.com>
2019-11-09 18:06:39 +0800

    update

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = 愛你
+    s = '愛你'
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=s))
